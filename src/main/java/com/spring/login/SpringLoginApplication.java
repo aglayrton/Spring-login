61693540ea99b5354b9d1c580af8d5af1e339b48
Aglayrton Julião <aglayrtonjuliao@gmai.com>
2020-11-15 17:02:11 -0300

    Projeto de login e senha de usuários inicializada

---
+++ src/main/java/com/spring/login/SpringLoginApplication.java
@@ -0,0 +1,13 @@
+package com.spring.login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLoginApplication.class, args);
+	}
+
+}

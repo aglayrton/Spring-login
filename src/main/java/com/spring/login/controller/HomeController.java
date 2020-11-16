03c2743456fa3bfe4fb5f4a87aac4bfced56f43a
Aglayrton Julião <aglayrtonjuliao@gmai.com>
2020-11-15 23:57:09 -0300

    Métodos de acesso login, loginError e home

--- src/main/java/com/spring/login/controller/HomeController.java
+++ src/main/java/com/spring/login/controller/HomeController.java
@@ -1,13 +1,28 @@
 package com.spring.login.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 	
-	@GetMapping("/")
+	@GetMapping({"/", "/home"})
 	public String home() {
 		return "index";
+	}
+	
+	@GetMapping({"/login"})
+	public String login() {
+		return "login";
+	}
+	
+	@GetMapping({"/login-error"})
+	public String loginError(ModelMap model) {
+		model.addAttribute("alerta", "erro");
+		model.addAttribute("titulo", "Credenciais inválidas");
+		model.addAttribute("texto", "Login ou senha incorretos, tente novamente");
+		model.addAttribute("subtexto", "Acesso permitido apenas para cadastros já ativados");
+		return "login";
 	}
 }

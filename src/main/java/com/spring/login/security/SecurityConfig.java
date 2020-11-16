2eb8fbc3800fab2232832bb6ca1b38630511ff63
Aglayrton Julião <aglayrtonjuliao@gmai.com>
2020-11-16 00:29:06 -0300

    Método de autenticação para a validação, e neste o método para usar a criptografia escolhida

--- src/main/java/com/spring/login/security/SecurityConfig.java
+++ src/main/java/com/spring/login/security/SecurityConfig.java
@@ -1,11 +1,19 @@
 package com.spring.login.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.spring.login.service.UsuarioService;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	@Autowired
+	UsuarioService service;
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -27,4 +35,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 			.logoutSuccessUrl("/");
 	}
 	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(service).passwordEncoder(new BCryptPasswordEncoder());
+	}
 }
